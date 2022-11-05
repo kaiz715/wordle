c71816febec59fe173e55a41d2366eae499dcc1b
Kai Zheng <59148999+kaiz715@users.noreply.github.com>
2022-11-05 15:46:58 -0400

    fix stuffs

--- main.py
+++ main.py
@@ -3,32 +3,7 @@
 import words
 import random
 
-import pygame
-
-WIDTH, HEIGHT = 900, 900
-FPS = 30
-
-WHITE = (255, 255, 255)
-GREEN = "#6aaa64"
-YELLOW = "#c9b458"
-GREY = "#787c7e"
-
-pygame.init()
-pygame.mixer.init()
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("wordle")
-clock = pygame.time.Clock()
-
-
-
-board = [
-    ["_","_","_","_","_"],
-    ["_","_","_","_","_"],
-    ["_","_","_","_","_"],
-    ["_","_","_","_","_"],
-    ["_","_","_","_","_"],
-    ["_","_","_","_","_"]
-]
+wordarray = [["_","_","_","_","_"],["_","_","_","_","_"],["_","_","_","_","_"],["_","_","_","_","_"],["_","_","_","_","_"],["_","_","_","_","_"]]
 
 
 def get_word():
@@ -40,25 +15,37 @@ word = get_word()
 def wordle(guess1):
   
   letters = ["_","_","_","_","_"]
+  coloredStr = ["_","_","_","_","_"]
   
   #green letters
   for a in range(5):
     if guess1[a:a+1]==word[a:a+1]:
       letters[a] = guess1[a:a+1].upper()
+      coloredStr[a] = '\033[0;37;42m' + guess1[a:a+1].upper() + '\033[0;0m'
   
   #yellow letters
   index = 0
   for y in guess1:
     if y in word and letters[index]== "_":
       letters[index] = y.upper()
+      coloredStr[index] = '\033[0;37;43m' + y.upper() + '\033[0;0m'
     index+=1
   
   #gray letters
   for b in range(5):
     if letters[b] == "_":
       letters[b] = guess1[b].upper()
+      coloredStr[b] = guess1[b].upper()
   
-  return letters
+  return coloredStr
+
+def displayBoard(arr):
+  for row in arr:
+    strtoprint = ""
+    for col in row:
+      strtoprint += col
+    print(strtoprint)
+  return
 
 def replaceWord(arr, guess):
   print("")
@@ -74,13 +61,6 @@ def replaceWord(arr, guess):
     arr[countOfRow][x] = wordledguess[x]
 
 
-def display_board():
-  screen.fill(WHITE)
-  for row in range(6):
-    for col in range(5):
-      pygame.draw.rect(screen,GREY, (col*WIDTH/5 + 10, row*HEIGHT/6+10, WIDTH/5-20, HEIGHT/5-20))
-  
-
 guess_count = 0
 gameRunning = True
 
@@ -88,30 +68,26 @@ print("Hello! This is BHS CS Club's Wordle game!")
 print("Made by Evelyn, Kai, Michael")
 while gameRunning == True:
   validGuess = False
+  guess = input()
+  while validGuess == False:
+    if guess not in words.validwords and guess not in words.wordlist:
+      print('not a valid word')
+      guess = input()
+    else:
+      validGuess = True
 
-  clock.tick(FPS)  
+  #clear the console
+  print("Hello! This is BHS CS Club's Wordle game!")
+  print("Made by Evelyn, Kai, Michael")
 
-  display_board()
-  pygame.display.flip()
+  replaceWord(wordarray, guess)
 
+  displayBoard(wordarray)
 
-  # guess = input()
-  # while validGuess == False:
-  #   if guess not in words.validwords and guess not in words.wordlist:
-  #     print('not a valid word')
-  #     guess = input()
-  #   else:
-  #     validGuess = True
-
-  # print("Hello! This is BHS CS Club's Wordle game!")
-  # print("Made by Evelyn, Kai, Michael")
-
-  # replaceWord(board, guess)
-
-  # guess_count+=1
-  # if guess == word:
-  #   print("Congrats! You win :))")
-  #   gameRunning = False
-  # elif guess_count > 5:
-  #   print("Sorry, you lose. The word was " + word + ".")
-  #   gameRunning = False
+  guess_count+=1
+  if guess == word:
+    print("Congrats! You win :))")
+    gameRunning = False
+  elif guess_count > 5:
+    print("Sorry, you lose. The word was " + word + ".")
+    gameRunning = False
